096a7e2a87049544dfd2bca675a60ff4ce40c6f3
Timothy Alhorn <tdalhorn@gmail.com>
2022-10-22 10:20:19 -0500

    Alhorn testing

--- src/main/java/org/XChangeIt/Main.java
+++ src/main/java/org/XChangeIt/Main.java
@@ -1,7 +1,15 @@
 package org.XChangeIt;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+
+        //Parts of the program:
+        //Load data
+        //store data
+        //process data
+        //conclude data
+        //repeat
     }
 }
