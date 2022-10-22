4f6e97e7c56553c6e458043ea95aacb043ac9a58
Timothy Alhorn <tdalhorn@gmail.com>
2022-10-22 11:09:37 -0500

    Savannah and I worked on this file. Version 1.01 of the DataStorage Class.

--- src/main/java/org/XChangeIt/Main.java
+++ src/main/java/org/XChangeIt/Main.java
@@ -1,10 +1,27 @@
 package org.XChangeIt;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import java.util.Arrays;
 
 public class Main
 {
+    //Private variable for the class
+    private DataCollector fetcher;
+    private DataStorage data;
+    private PreviousTransactions history;
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        String a = "1";
+        JFrame frame = new JFrame();
+        JButton englishButto
+        //JOptionPane.showMessageDialog(null, "Hello, World!");
+        //array of unknown length for the elements in the data file
+        //Sets up the private variables
+        DataStorage data = new DataStorage();
+
 
         //Parts of the program:
         //Load data
