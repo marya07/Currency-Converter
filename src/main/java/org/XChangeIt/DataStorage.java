d1b84880bca29df86ff47065c22f29646451227a
Timothy Alhorn <tdalhorn@gmail.com>
2022-10-22 10:53:13 -0500

    Savannah and I worked on this file. Version 1 of the DataStorage Class.

---
+++ src/main/java/org/XChangeIt/DataStorage.java
@@ -0,0 +1,43 @@
+package org.XChangeIt;
+
+public class DataStorage
+{
+    private String targetName;         //Holds the name for the other currency
+    private double exchangeRate;       //Note, rate is always in terms of 1 USD to other $$
+    private double inverseRate;        //Note, rate is always in terms of 1 USD to other $$
+
+    public void DataStorage(String name, double numE, double numI)
+    {
+        targetName = name;
+        exchangeRate = numE;
+        inverseRate = numI;
+    }
+
+    public void setName(String name)
+    {
+        targetName = name;
+    }
+    public String getName()
+    {
+        return targetName;
+    }
+    public void setExchangeRate(double numE)
+    {
+        exchangeRate = numE;
+    }
+    public double getExchangeRate()
+    {
+        return exchangeRate;
+    }
+    public void setInverseRate(double numI)
+    {
+        inverseRate = numI;
+    }
+    public double getInverseRate()
+    {
+        return inverseRate;
+    }
+
+
+
+}
