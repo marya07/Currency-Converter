c7bb60f7b0da28dcfd7780a463898908ccafca96
Timothy Alhorn <tdalhorn@gmail.com>
2022-11-05 13:09:28 -0500

    Update with new function to collect the profits from a file

--- src/main/java/org/XChangeIt/DataExtractor.java
+++ src/main/java/org/XChangeIt/DataExtractor.java
@@ -9,17 +9,51 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.util.Objects;
 
 public class DataExtractor {
     /**
-     * Creates a new DataExtractor object for extracting data from the given XML file conating rate data.
+     * Creates a new DataExtractor object for extracting data from the given XML file containing rate data.
      * @param file File variable initialized using XML file location to extract data from.
      * @param fromCurrency currency for which data needs to extracted for conversion.
      */
     public DataExtractor(File file, String fromCurrency){
         f = file;
         baseCurrency = fromCurrency;
+    }
+
+    /**
+     * Extraction method to get the current profit total from this project
+     * If -1 is returned then profits needs to be set to 0.0 of type double
+     * @return double variable with exact value of money made.
+     */
+    public double getProfits()// throws FileNotFoundException
+    {
+        double ProfitsCollected = -1;    //Variable to hold the profits collected. -1 to show not set. If it remains -1, there is a problem
+        //Creates a new File variable to hold the Profits.xml file
+        File info = new File("src/main/resources/Profits.xml");    //Opens the file
+        //Tries to load the data
+        try
+        {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(info);
+            NodeList list = doc.getElementsByTagName("ProjectProfits");
+
+            Node n = list.item(0);
+            Element e = (Element) n;
+            ProfitsCollected = Double.parseDouble(e.getElementsByTagName("ProfitSoFar").item(0).getTextContent());
+        }
+        //Catches the exception
+        catch (Exception e)
+        {
+            e.getCause();
+            e.printStackTrace(System.out);
+        }
+        //Returns the collected profits
+        return ProfitsCollected;
     }
 
     /**
