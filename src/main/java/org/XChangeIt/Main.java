cd10a494bc9cbb74a31c43737f95e0f5057aec9c
Danial-Ahari <93231570+Danial-Ahari@users.noreply.github.com>
2022-11-05 11:02:37 -0500

    Deleted ConversionList and adjusted the commented test code in Main appropriately. We do not need ConversionList because we chose to go in a different direction.

--- src/main/java/org/XChangeIt/Main.java
+++ src/main/java/org/XChangeIt/Main.java
@@ -1,6 +1,7 @@
 package org.XChangeIt;
 
 // This was used for testing. It might be needed later. Otherwise, delete. - Danial Ahari
+import org.XChangeIt.models.Conversion;
 import org.XChangeIt.models.Receipt;
 import org.XChangeIt.models.TransactionList;
 
@@ -9,9 +10,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         // This bit of code briefly tested to see if basic features of Conversion and ConversionList were working.
-        // ConversionList List = new ConversionList();
-        // List.addNewConversion("USD","CFA",659.90,0.0015);
-        // System.out.println(List.getConversionByTargetCurrency("CFA").getExchangeRate());
+        // Conversion conv = new Conversion("USD","CFA",659.90,0.0015);
+        // System.out.println(conv.getExchangeRate());
         //
         // And this bit of code briefly tested Transaction and TransactionList.
         // TransactionList List = new TransactionList();
