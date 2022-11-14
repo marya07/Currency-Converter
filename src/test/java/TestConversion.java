e54797045124169ad19af3bf012cefd3f1c80347
megharya <aryamegh07@gmail.com>
2022-11-13 20:12:08 -0600

    Added Money Class

--- src/test/java/TestConversion.java
+++ src/test/java/TestConversion.java
@@ -1,19 +1,25 @@
 import org.XChangeIt.Converter;
+import org.XChangeIt.models.Money;
 
 import java.util.Scanner;
 
 public class TestConversion {
     public static void main(String[] args) {
-        String fromCurrency;
-        int amount;
+        try {
+            String fromCurrency;
+            int amount;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Currency Type: ");
-        fromCurrency = scanner.nextLine();
-        System.out.println("Enter Currency Amount: ");
-        amount = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Currency Type: ");
+            fromCurrency = scanner.nextLine();
+            System.out.println("Enter Currency Amount: ");
+            amount = scanner.nextInt();
 
-        Converter c = new Converter(fromCurrency, amount);
-        System.out.println("Your Amount: " + c.doConvert());
+            Converter c = new Converter(fromCurrency, amount);
+            Money m = c.doConvert();
+            System.out.println("Your Amount: " + m.getCurrencyType() + " " + m.getAmount());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
