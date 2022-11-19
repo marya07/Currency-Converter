6e1b1305bc1c5071cbfe53e7fc4356c335506da2
Timothy Alhorn <tdalhorn@gmail.com>
2022-11-19 12:35:06 -0600

    Update with new function to collect the profits from a file

--- src/main/java/org/XChangeIt/models/Receipt.java
+++ src/main/java/org/XChangeIt/models/Receipt.java
@@ -6,21 +6,31 @@ public class Receipt {
     /**
      * Constructs a new receipt object to store the receipt.
      * @param trans Transaction object to make the receipt from.
+     * @param fees Double variable to hold the amount charged in fees for this transaction
      */
-    public Receipt(Transaction trans) {
+    public Receipt(Transaction trans, double fees) {
         // The formatting of "receiptText" here is a placeholder. Change if need-be.
-        this.receiptText = "Your Transaction Information:" +
-                "\n\nTransaction date and time: " + trans.getTransactionTime().toString() +
-                "\nCurrency Deposited: " + trans.getStartingAmount() + trans.getStartingCurrency() +
-                "\nCurrency Withdrawn: " + trans.getEndingAmount() + trans.getEndingCurrency() +
+        this.receiptText = "Receipt:" +
+                "\n\nTransaction Date and Time: \t\t" + trans.getTransactionTime().toString() +
+                "\nStarting Currency and Amount: \t\t" + trans.getStartingAmount() + " " + trans.getStartingCurrency() +
+                "\nEnding Currency and Amount: \t\t" + trans.getEndingAmount() + " " + trans.getEndingCurrency() +
+                "\nTransaction Fees: \t\t\t\t" + fees + " " + trans.getEndingCurrency() +
                 "\n\nThank you for doing business with us. Please come again.";
     }
 
     /**
-     * Function to "print" the receipt.
+     * Function to "print" the receipt. Prints the receipt to a file to simulate printing through a printer
      */
     public void printReceipt() {
         // Replace this with code to output to file later.
         System.out.print(receiptText);
+    }
+
+    /**
+     * Function to return the receipt text. Useful for GUI
+     */
+    public String getReceiptTest()
+    {
+        return receiptText;
     }
 }
